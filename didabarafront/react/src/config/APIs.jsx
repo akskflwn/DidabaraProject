d84c2d2fcdaafee42a46995ff245d617944d025f
powercording <adoim@naver.com>
2022-09-02 15:25:39 +0900

    i want to eat a sushi

--- didabarafront/react/src/config/APIs.jsx
+++ didabarafront/react/src/config/APIs.jsx
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const REQUEST_ADDRESS1 = "http://192.168.0.187:8080/";
 const REQUEST_ADDRESS2 = "http://192.168.0.48:8080/";
+const REQUEST_ADDRESS3 = "http://localhost:8080/";
 
-export const REQUEST_ADDRESS = REQUEST_ADDRESS1;
+export const REQUEST_ADDRESS = REQUEST_ADDRESS3;
 
 export const getUserData = () => {
   console.log("query working....");
